e608e0b7047f1b23ffc64c29802564b9b484aa9b
trealtye20 <exantustyrese10@gmail.com>
2021-12-11 21:27:49 -0500

    employee

--- index.js
+++ index.js
@@ -125,6 +125,8 @@ async function viewEmployeesByDepartment() {
     // CREATE TWO PROPERTIES name AND value FOR THIS OBJECT. THE PROPERTY name SHOULD CONTAIN THE NAME OF THE DEPARTMENT.
     // THE PROPERTY value SHOULD CONTAIN id.
     // TODO: YOUR CODE HERE
+    name: name,
+    value: id,
 
   }));
 
@@ -153,6 +155,8 @@ async function updateEmployeeRole() {
     // THE PROPERTY value SHOULD CONTAIN id.
     // THIS OBJECT FOR EACH MANAGER WILL RETURN TO MAP() TO CONSTRUCT AN ARRAY TO BE RETURNED AND BE STORED TO managerChoices.
     // TODO: YOUR CODE HERE
+    name: first_name, last_name,
+    value: id,
 
   }));
 
@@ -287,6 +291,8 @@ async function addEmployee() {
     // THE PROPERTY value SHOULD CONTAIN id.
     // THIS OBJECT FOR EACH MANAGER WILL RETURN TO MAP() TO CONSTRUCT AN ARRAY TO BE RETURNED AND BE STORED TO managerChoices.
     // TODO: YOUR CODE HERE
+    name: first_name, last_name,
+    value: id,
 
   }));
   managerChoices.unshift({ name: "None", value: null });
